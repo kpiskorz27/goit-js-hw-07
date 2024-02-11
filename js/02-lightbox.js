a4c0c5224f66c5bb2ab614d96917cfd0bf216274
kpiskorz27 <kpiskorz@gmx.at>
2024-02-11 17:58:19 +0100

    HW 7 - 2.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,21 @@
 import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+const imagesElement = galleryItems
+  .map(
+    (item) =>
       `<li>
+      <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a></li>`
+  )
+  .join("");
+
+galleryElement.insertAdjacentHTML("afterbegin", imagesElement);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
